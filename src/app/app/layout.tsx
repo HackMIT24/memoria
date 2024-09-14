fc18491f5bb343ef64fc966348f11f823d1fa652
Arnab Ghosh <arnabcare21@gmail.com>
2024-09-14 19:10:51 -0400

    Fix a few bugs, improve the styling.

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,7 +1,13 @@
+import {api} from "../../../convex/_generated/api";
+import {useQuery} from "convex/react";
+
 export default function AppLayout({ children }: { children: React.ReactNode}) {
+
+	const user = useQuery(api.user.default);
+
 	return (
-		<>
+		<div>
 			{ children }
-		</>
+		</div>
 	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<div className="m-5 p-5 w-full h-screen flex flex-col justify-around bg-gradient-to-br from-blue-100 to-red-100">
+		<div className="m-5 p-5 w-full h-screen flex flex-col justify-around">
 			<h1 className="text-9xl text-blue-900">Memoria</h1>
 			<p>Manage your loved one's Alzheimer's <span className={'text-blue-600'}> without stigma.</span></p>
 		</div>
