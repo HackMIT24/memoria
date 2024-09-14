0c114f640b35296dc176f7f199f1cf1ee144b496
Arnab Ghosh <arnabcare21@gmail.com>
2024-09-14 17:39:49 -0400

    Connect Convex and Clerk up together

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,7 +1,17 @@
+'use client'
+
+import {useQuery} from "convex/react";
+import {api} from "../../../convex/_generated/api";
+
 export default function AppPage() {
+
+	const tasks = useQuery(api.user.default)
+
 	return (
 		<>
 			<h1 className={"text-2xl"}>Welcome to Memoria!</h1>
+			{/* Infodumping right now to test some hooks */}
+			{ tasks?.toString() }
 		</>
 	)
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
+'use client'
+
 import {
 	ClerkProvider,
 	SignInButton,
 	SignedIn,
 	SignedOut,
-	UserButton
+	UserButton, useAuth
 } from '@clerk/nextjs'
 import './globals.css'
 import Navbar from "@/components/Navbar";
 import { Poppins } from 'next/font/google';
+import {ConvexProviderWithClerk} from "convex/react-clerk";
+import {ConvexReactClient} from "convex/react";
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
 
 const poppins = Poppins({
 	subsets: ['latin'],
 	style: "normal",
 	weight: '400',
 })
-
-export const metadata = {
-	title: "Memoria",
-	description: "Memoria is an app to help manage patient's Alzhemier's symptoms."
-}
-
 
 export default function RootLayout({
 									   children,
@@ -28,14 +28,16 @@ export default function RootLayout({
 }) {
 	return (
 		<ClerkProvider>
-			<html lang="en">
-				<body className={`p-3 ${poppins.className}`}>
-					<Navbar />
-					<div className="p-3">
-						{children}
-					</div>
-				</body>
-			</html>
+			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+				<html lang="en">
+					<body className={`p-3 ${poppins.className}`}>
+						<Navbar />
+						<div className="p-3">
+							{children}
+						</div>
+					</body>
+				</html>
+			</ConvexProviderWithClerk>
 		</ClerkProvider>
 	)
 }
