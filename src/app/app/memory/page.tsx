352fffeb1bcd8a2317a8104cb6b2c609cd051ef0
ecc2205@columbia.edu <ecc2205@columbia.edu>
2024-09-15 00:43:53 -0400

    memory page.tsx fix

--- src/app/app/memory/page.tsx
+++ src/app/app/memory/page.tsx
@@ -3,12 +3,19 @@
 import CardInputForm from "@/components/carditemform";
 
 export default function MemoryPage() {
+  const handleAddCard = (card: any) => {
+    // Implement the logic for adding a card
+    console.log('Card added:', card);
+  };
 
+  const handleFinish = () => {
+    // Implement the logic for finishing
+    console.log('Finished');
+  };
 
-
-	return (
-		<div>
-			<CardInputForm onAddCard={} onFinish={} />
-		</div>
-	)
+  return (
+    <div>
+      <CardInputForm onAddCard={handleAddCard} onFinish={handleFinish} />
+    </div>
+  );
 }
