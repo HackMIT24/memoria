17e008b8dbe010af70dde0e8a929544bf9e1d6d0
Arnab Ghosh <arnabcare21@gmail.com>
2024-09-15 00:02:50 -0400

    Add memories ts file

--- src/app/app/memory/page.tsx
+++ src/app/app/memory/page.tsx
@@ -1,9 +1,11 @@
 'use client'
 
-import MemoryGame from "@/components/memorygame";
 import CardInputForm from "@/components/carditemform";
 
 export default function MemoryPage() {
+
+
+
 	return (
 		<div>
 			<CardInputForm onAddCard={} onFinish={} />
